3813466e32ed8b6de2d1b148cbfb50e7984b02e4
ndoherty4 <122536107+ndoherty4@users.noreply.github.com>
2023-06-01 18:17:42 +0100

    Delete graduate-challenge directory

--- graduate-challenge/java/app/src/main/java/hangman/interfaces/IdentifierGeneration.java
+++
@@ -1,7 +0,0 @@
-package hangman.interfaces;
-
-import java.util.UUID;
-
-public interface IdentifierGeneration {
-    UUID retrieveIdentifier();
-}
--- graduate-challenge/java/app/src/main/java/hangman/models/Guess.java
+++
@@ -1,9 +0,0 @@
-package hangman.models;
-
-public class Guess {
-    private String letter;
-
-    public String getLetter() {
-        return letter;
-    }
-}
